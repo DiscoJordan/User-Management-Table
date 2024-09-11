d227b5ccbf66a0fc3657c98c5463a2f0126ddba8
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-09-11 22:29:23 +0200

    updated

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,6 +7,5 @@ export const store = configureStore({
   },
 });
 
-// Определите тип для использования в компонентах
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
