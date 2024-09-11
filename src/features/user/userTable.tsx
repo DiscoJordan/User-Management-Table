c45ee00ef043afcdb8d1ea4efcd5c8e2800c6fb5
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-09-11 22:26:51 +0200

    updated interface

--- src/features/user/userTable.tsx
+++ src/features/user/userTable.tsx
@@ -34,52 +34,83 @@ const UserTable: React.FC = () => {
           <thead className="table-head">
             <tr className="head-row">
               <th>
-                Name
-                <input
-                  className="search"
-                  name="name"
-                  value={filters.name}
-                  onChange={handleFilterChange}
-                  placeholder="Search by name"
-                />
+                <div className="title">
+                  <span className="material-symbols-outlined">badge</span>
+                  Name
+                </div>
+                <br />
+                <div className="search-input">
+                  <span className="material-symbols-outlined">search</span>
+                  <input
+                    className="search"
+                    name="name"
+                    value={filters.name}
+                    onChange={handleFilterChange}
+                    placeholder="Search by name"
+                  />
+                </div>
               </th>
               <th>
-                Username
-                <input
-                  className="search"
-                  name="username"
-                  value={filters.username}
-                  onChange={handleFilterChange}
-                  placeholder="Search by username"
-                />
+                <div className="title">
+                  <span className="material-symbols-outlined">
+                    account_circle
+                  </span>
+                  Username
+                </div>
+                <br />
+                <div className="search-input">
+                  <span className="material-symbols-outlined">search</span>
+                  <input
+                    className="search"
+                    name="username"
+                    value={filters.username}
+                    onChange={handleFilterChange}
+                    placeholder="Search by username"
+                  />
+                </div>
               </th>
               <th>
-                Email
-                <input
-                  className="search"
-                  name="email"
-                  value={filters.email}
-                  onChange={handleFilterChange}
-                  placeholder="Search by email"
-                />
+                <div className="title">
+                  <span className="material-symbols-outlined">mail</span>
+                  Email
+                </div>
+                <br />
+                <div className="search-input">
+                  <span className="material-symbols-outlined">search</span>
+                  <input
+                    className="search"
+                    name="email"
+                    value={filters.email}
+                    onChange={handleFilterChange}
+                    placeholder="Search by email"
+                  />
+                </div>
               </th>
               <th>
-                Phone
-                <input
-                  className="search"
-                  name="phone"
-                  value={filters.phone}
-                  onChange={handleFilterChange}
-                  placeholder="Search by phone"
-                />
+                <div className="title">
+                  <span className="material-symbols-outlined">call</span>
+                  Phone
+                </div>
+                <br />
+                <div className="search-input">
+                  <span className="material-symbols-outlined">search</span>
+                  <input
+                    className="search"
+                    name="phone"
+                    value={filters.phone}
+                    onChange={handleFilterChange}
+                    placeholder="Search by phone"
+                  />
+                </div>
               </th>
             </tr>
           </thead>
           <tbody className="table-body">
             {filteredUsers.length ? (
-              filteredUsers.map((user) => (
+              filteredUsers.map((user,i) => (
+             
                 <tr className="body-row" key={user.id}>
-                  <td>{user.name}</td>
+                  <td>{++i}. {user.name}</td>
                   <td>{user.username}</td>
                   <td>{user.email}</td>
                   <td>{user.phone}</td>
